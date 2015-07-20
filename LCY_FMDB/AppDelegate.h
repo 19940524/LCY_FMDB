a6b76bad85e2379a38ea61530ec86b3488f160e8
GuoBin <1664880373@qq.com>
2015-07-20 21:22:11 +0800

    Initial Commit

---
+++ LCY_FMDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCY_FMDB
+//
+//  Created by GuoBIn on 15/7/20.
+//  Copyright (c) 2015年 自强不息. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCY_FMDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCY_FMDB
+//
+//  Created by GuoBIn on 15/7/20.
+//  Copyright (c) 2015年 自强不息. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
